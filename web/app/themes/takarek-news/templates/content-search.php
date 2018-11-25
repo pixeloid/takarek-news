a1344608ec9fb7910ca5810efcf31c93f7804e72
Gergely Olah <olah.gergely@pixeloid.hu>
2018-11-25 13:51:55 +0100

    search form

--- web/app/themes/takarek-news/templates/content-search.php
+++ web/app/themes/takarek-news/templates/content-search.php
@@ -1,9 +1,19 @@
 <article <?php post_class(); ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php if (get_post_type() === 'post') { get_template_part('templates/entry-meta'); } ?>
-  </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
-  </div>
+	<div class="grid-x grid-margin-x align-middle ">
+		<div class="cell shrink">
+			<?php the_post_thumbnail('thumbnail'); ?>
+		</div>
+		<div class="cell auto">
+			<header class="">
+				<h2 class="h5 entry-title">
+					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+				</h2>
+			</header>
+			<div class="entry-summary ">
+				<?php the_excerpt(); ?>
+			</div>
+			<a href="<?php the_permalink() ?>" class="button">Tovább</a>
+
+		</div>
+	</div>
 </article>
