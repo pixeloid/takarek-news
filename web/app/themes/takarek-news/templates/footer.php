a05a0c9af67330708ae709d5755813ab8ef4ab53
Gergely Olah <olah.gergely@pixeloid.hu>
2018-11-30 12:19:47 +0100

    remove old theme

--- web/app/themes/takarek-news/templates/footer.php
+++
@@ -1,8 +0,0 @@
-<footer class="site-footer">
-  <div class="grid-container fluid">
-  	<div class="grid-x align-middle ">
-
-	    <?php dynamic_sidebar('sidebar-footer'); ?>
-	</div>
-  </div>
-</footer>
